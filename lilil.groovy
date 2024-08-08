d14b813f92e4cf2659e32a6279b9474dabb83756
T G <tg@Ts-MacBook-Air.local>
2024-08-08 11:50:32 +0300

    lala

--- lilil.groovy
+++ lilil.groovy
@@ -1,13 +1,14 @@
 // Fetch the latest changes from the remote repository
-sh(script: 'git fetch')
+'git fetch'.execute()
 
 // Check if the specific folder has changed
-def changes = sh(script: 'git diff --name-only HEAD HEAD~1 -- ./test', returnStdout: true).trim()
+def changes = 'git diff --name-only HEAD HEAD~1 -- ./test'.execute()
+println changes.text
 
-if (changes) {
-    echo "Changes detected in the folder."
-    // You can set a variable or take any action based on the changes
-} else {
-    echo "No changes detected in the folder."
-    // You can skip subsequent steps or handle accordingly
-}
+// if (changes) {
+//     echo "Changes detected in the folder."
+//     // You can set a variable or take any action based on the changes
+// } else {
+//     echo "No changes detected in the folder."
+//     // You can skip subsequent steps or handle accordingly
+// }
