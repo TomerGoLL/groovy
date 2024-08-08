51b709620b8b567a3ee94eb66bdeee5bc0be88b0
T G <tg@Ts-MacBook-Air.local>
2024-08-08 11:45:40 +0300

    add test folder

--- lilil.groovy
+++ lilil.groovy
@@ -1,8 +1,13 @@
-println "hi"
+// Fetch the latest changes from the remote repository
+sh(script: 'git fetch')
 
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/TomerGoLL/groovy.git
-git push -u origin main
+// Check if the specific folder has changed
+def changes = sh(script: 'git diff --name-only HEAD HEAD~1 -- ./test', returnStdout: true).trim()
+
+if (changes) {
+    echo "Changes detected in the folder."
+    // You can set a variable or take any action based on the changes
+} else {
+    echo "No changes detected in the folder."
+    // You can skip subsequent steps or handle accordingly
+}
